adf5e67c3c112a30ed508ec8e9754940f5401442
Hafiz <komefumi@protonmail.ch>
2024-01-12 11:28:49 +0530

    Quick login page/form

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	let email = '';
+	function handleLogin() {
+		console.log({ email });
+	}
+</script>
+
+<div class="flex pt-20 flex-col justify-center px-6 lg:px-8">
+	<div class="sm:mx-auto sm:w-full sm:max-w-sm">
+		<h2 class="text-center text-2xl font-bold leading-9 tracking-tight">Sign in to your account</h2>
+	</div>
+
+	<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+		<form class="space-y-6" action="#" method="POST" on:submit|preventDefault={handleLogin}>
+			<div>
+				<label for="email" class="block text-sm font-medium leading-6">Email address</label>
+				<div class="mt-2">
+					<input
+						id="email"
+						name="email"
+						type="email"
+						autocomplete="email"
+						required
+						bind:value={email}
+						class="block w-full text-gray-900 rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+					/>
+				</div>
+			</div>
+
+			<div>
+				<button
+					type="submit"
+					class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					>Sign in</button
+				>
+			</div>
+		</form>
+	</div>
+</div>
