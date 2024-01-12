1869491a7db1646b3adf4175303731e954b768a3
Hafiz <komefumi@protonmail.ch>
2024-01-12 12:27:12 +0530

    Adds supabase

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
